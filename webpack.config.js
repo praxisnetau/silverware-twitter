3e2b7b2a09bf218750c29323d4f98c93239bd0ec
Colin Tucker <colin@praxis.net.au>
2018-06-12 17:55:23 +1000

    Updated dependencies and base resources path.

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 // Define Base:
 
-const BASE = '/resources/silverware/twitter';
+const BASE = '/resources/vendor/silverware/twitter';
 
 // Define Paths:
 
